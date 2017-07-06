c3178c669ae5b89ef9309153eeeb4f59816d47f3
JuanFerrer <juanemilioferrer@outlook.com>
2017-07-06 16:21:56 +0100

    Number selection and modification

--- js/index.js
+++ js/index.js
@@ -7,3 +7,46 @@ $(function () {
         $(this).addClass("ui-selected");
     });
 })
+
+document.addEventListener("keydown", function (e) {
+    // Not pressed Control
+    if (e.keyCode !== 17) {
+
+        switch (e.keyCode) {
+            case 49: case 97:
+                num = "1";
+                break;
+            case 50: case 98:
+                num = "2";
+                break;
+            case 51: case 99:
+                num = "3";
+                break;
+            case 52: case 100:
+                num = "4";
+                break;
+            case 53: case 101:
+                num = "5";
+                break;
+            case 54: case 102:
+                num = "6";
+                break;
+            case 55: case 103:
+                num = "7";
+                break;
+            case 56: case 104:
+                num = "8";
+                break;
+            case 57: case 105:
+                num = "9";
+                break;
+            default:
+                num = "-";
+                break;
+        }
+        $(".square.ui-selected").each(function () {
+            $(this).html(num);
+            $(this).removeClass("ui-selected");
+        })
+    }
+})
