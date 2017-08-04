7ecd1903c83463f3e6d73cc80ba1f83f4adeb09c
JuanFerrer <juanemilioferrer@outlook.com>
2017-08-04 17:24:58 +0100

    Better CSS

---
+++ js/functions.js
@@ -0,0 +1,77 @@
+/**
+ * Deselect all
+ */
+function clearSelection() {
+    $(".square.ui-selected").each(function () {
+        $(this).removeClass("ui-selected");
+    })
+}
+
+/**
+ * Check the board and mark conflicts
+ */
+function checkBoard() {
+    $(".conflict").removeClass("conflict");
+    var cols = [];
+    var rows = [];
+    for (var i = 0; i < 9; ++i) {
+        rows = $(".row" + i).toArray();
+        for (var j = 0; j < 9; ++j) {
+            for (var k = 0; k < 9; ++k) {
+                // console.log(rows[k].innerHTML + "===" + rows[j].innerHTML);
+                if (rows[j].innerHTML === rows[k].innerHTML &&
+                    rows[j].innerHTML !== emptyChar &&
+                    rows[k].innerHTML !== emptyChar &&
+                    j !== k) {
+                    rows[j].classList.add("conflict");
+                    rows[k].classList.add("conflict");
+                }
+            }
+        }
+        cols = $(".col" + i).toArray();
+        for (j = 0; j < 9; ++j) {
+            for (k = 0; k < 9; ++k) {
+                if (cols[j].innerHTML === cols[k].innerHTML &&
+                    cols[j].innerHTML !== emptyChar &&
+                    cols[k].innerHTML !== emptyChar &&
+                    j !== k) {
+                    cols[j].classList.add("conflict");
+                    cols[k].classList.add("conflict");
+                }
+            }
+        }
+    }
+    var squares = [];
+    for (i = 0; i < 9; ++i) {
+        squares = $(".square" + i).children().toArray();
+        for (j = 0; j < 9; ++j) {
+            for (k = j + 1; k < 9; ++k) {
+                if (squares[j].innerHTML === squares[k].innerHTML &&
+                    squares[j].innerHTML !== emptyChar &&
+                    squares[k].innerHTML !== emptyChar) {
+                    squares[j].classList.add("conflict");
+                    squares[k].classList.add("conflict");
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Clear the board
+ */
+function clearBoard() {
+    //$(".square").html(emptyChar);
+    $(".square").removeClass("conflict");
+    //$(".square").html(function () { return p[num++]; });
+    for (var i = 0, num = 0; i < 9; ++i) {
+        $(".row" + i).html(function () { return p[num++]; });
+    }
+}
+
+/**
+ * Brute force the puzzle
+ */
+function solve() {
+    
+}
--- js/index.js
+++ js/index.js
@@ -10,6 +10,9 @@ $(function () {
     });
 })
 
+/**
+ * Adding event listeners
+ */
 document.addEventListener("keydown", function (e) {
     // Not pressed Control
     if (e.keyCode !== 17) {
@@ -53,61 +56,8 @@ document.addEventListener("keydown", function (e) {
     }
 })
 
-function checkBoard() {
-    $(".conflict").removeClass("conflict");
-    var cols = [];
-    var rows = [];
-    for (var i = 0; i < 9; ++i) {
-        rows = $(".row" + i).toArray();
-        for (var j = 0; j < 9; ++j) {
-            for (var k = 0; k < 9; ++k) {
-                // console.log(rows[k].innerHTML + "===" + rows[j].innerHTML);
-                if (rows[j].innerHTML === rows[k].innerHTML &&
-                    rows[j].innerHTML !== emptyChar &&
-                    rows[k].innerHTML !== emptyChar &&
-                    j !== k) {
-                    rows[j].classList.add("conflict");
-                    rows[k].classList.add("conflict");
-                }
-            }
-        }
-        cols = $(".col" + i).toArray();
-        for (j = 0; j < 9; ++j) {
-            for (k = 0; k < 9; ++k) {
-                if (cols[j].innerHTML === cols[k].innerHTML &&
-                    cols[j].innerHTML !== emptyChar &&
-                    cols[k].innerHTML !== emptyChar &&
-                    j !== k) {
-                    cols[j].classList.add("conflict");
-                    cols[k].classList.add("conflict");
-                }
-            }
-        }
-    }
-    var squares = [];
-    for (i = 0; i < 9; ++i) {
-        squares = $(".square" + i).children().toArray();
-        for (j = 0; j < 9; ++j) {
-            for (k = j + 1; k < 9; ++k) {
-                if (squares[j].innerHTML === squares[k].innerHTML &&
-                    squares[j].innerHTML !== emptyChar &&
-                    squares[k].innerHTML !== emptyChar) {
-                    squares[j].classList.add("conflict");
-                    squares[k].classList.add("conflict");
-                }
-            }
-        }
-    }
-}
+document.getElementsByTagName("html")[0].addEventListener("click", function () {
+    clearSelection();
+});
 
 var p = "003020600900305001001806400008102900700000008006708200002609500800203009005010300".replace(/0/g, emptyChar);
-
-function clearBoard() {
-    //$(".square").html(emptyChar);
-    $(".square").removeClass("conflict");
-    var num = 0;
-    //$(".square").html(function () { return p[num++]; });
-    for (var i = 0; i < 9; ++i) {
-        $(".row" + i).html(function () { return p[num++]; });
-    }
-}
